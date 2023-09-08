decc8553d9fe4548014559aeafd60a7f52ae7093
verygoodtuna <kjy990504@gmail.com>
2023-09-08 18:13:02 +0900

    nun

---
+++ js/tuna.js
@@ -0,0 +1,13 @@
+new fullpage('#main', {
+    anchors: ['main_page01', 'p02', 'p03', 'footer'],
+    css3: false,
+    scrollOverflow: false,
+});
+
+var swiper = new TOPSLIDE(".top_slide", {
+
+})
+
+var swiper = new BOTTOMSLIDE(".bottom_slide", {
+
+})
